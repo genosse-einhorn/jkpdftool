3c0f56faf89431b7ebd74595ce98e90d2a9c43c7
Jonas Kümmerlin <jonas@kuemmerlin.eu>
2019-06-16 22:10:18 +0200

    add ghostscript-based reencode tool

--- Makefile
+++ Makefile
@@ -9,13 +9,18 @@ CFLAGS_PKG     != $(PKGCONFIG) --cflags $(PKGS)
 LIBS           := -lm
 LIBS_PKG       != $(PKGCONFIG) --libs $(PKGS)
 
-EXE            := out/jkpdftool-pagefit out/jkpdftool-rotate out/jkpdftool-nup out/jkpdftool-splice out/jkpdftool-crop out/jkpdftool-ndown out/jkpdftool-overlay out/jkpdftool-rasterize
+EXE            := out/jkpdftool-pagefit out/jkpdftool-rotate out/jkpdftool-nup out/jkpdftool-splice out/jkpdftool-crop out/jkpdftool-ndown out/jkpdftool-overlay out/jkpdftool-rasterize out/jkpdftool-reencode
 
 all: $(EXE)
 
 out/%: %.c $(wildcard *.h) Makefile
 	@mkdir -p out
 	$(CC) -std=c11 $(CFLAGS) $(CFLAGS_PKG) -o $@ $< $(LIBS) $(LIBS_PKG)
+
+out/jkpdftool-reencode: jkpdftool-reencode.sh Makefile
+	@mkdir -p out
+	cp $< $@
+	chmod u+x $@
 
 clean:
 	rm -f $(EXE)
---
+++ jkpdftool-reencode.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ "$#" -gt 0 ]; then
+    (
+    printf 'Usage:\n'
+    printf '  %s <INPUT-PDF >OUTPUT-PDF\n' "$(basename "$0")"
+    printf '\n'
+    printf 'Recreate the PDF file using GhostScript. This can be a viable workaround\n'
+    printf 'when the other tools won’t process your PDF file correctly (which is considered\n'
+    printf 'a bug, but poppler isn’t always that fast in fixing them and maybe you don’t\n'
+    printf 'want to send them your PDF file but just want to get on with your life)\n'
+    ) 1>&2
+    exit 1
+fi
+
+if [ -t 0 ]; then
+    printf 'ERROR: refusing to read PDF from TTY\n' 1>&2
+    exit 1
+fi
+
+if [ -t 1 ]; then
+    printf 'ERROR: refusing to write PDF to TTY\n' 1>&2
+    exit 1
+fi
+
+if ! command -v gs >/dev/null 2>&1; then
+    printf 'ERROR: need ‘gs’ command line utility, please install GhostScript.\n' 1>&2
+    exit 1
+fi
+
+exec gs -sDEVICE=pdfwrite -q -o - -
